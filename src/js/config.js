5ebb9c6589b831b928aa0e2e99f0e270411399c5
Gabe Maze-Rogers <gmazerogers@gmail.com>
2017-03-16 16:40:34 -0700

    fixed service worker

--- src/js/config.js
+++ src/js/config.js
@@ -16,3 +16,15 @@ var collectionRef = db.ref('collections');
 var userRef = db.ref('user');
 var beerDatabaseRef = db.ref('beers');
 var wishlistRef = db.ref('wishlist');
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
 
 var BEER_STYLES = ['Pale Ale', 'Lager', 'IPA', 'Wheat', 'Belgian', 'Porter', 'Stout', 'Sour', 'Other'];
 
