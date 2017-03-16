c0e9efb93c6f4a3cf14867aa8e6072335d3ce5ab
Gabe Maze-Rogers <gmazerogers@gmail.com>
2017-03-16 16:44:33 -0700

    service worker still isnt working cmon clarence

--- src/js/config.js
+++ src/js/config.js
@@ -19,7 +19,7 @@ var wishlistRef = db.ref('wishlist');
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function(err) {
