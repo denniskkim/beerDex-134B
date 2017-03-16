509f920bce282cd0082ef1339f3d7fd86dd30758
Clarence Nguyen <clarrynguyen@gmail.com>
2017-03-15 22:30:13 -0700

    service worker adding

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
 var BEER_STYLES = ['Pale Ale', 'Lager', 'IPA', 'Wheat', 'Belgian', 'Porter', 'Stout', 'Sour', 'Other'];
 (function checkUserExists() {
     firebase.auth().onAuthStateChanged(function(user) {
